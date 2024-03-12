fba12115b9b849232f160cbbefca469f8f3e4cbe
CoyleWasHere <dancoyle321@gmail.com>
2024-03-12 10:06:53 -0400

    Initial Commit

---
+++ Beat The Deck/Beat_The_DeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  Beat_The_DeckApp.swift
+//  Beat The Deck
+//
+//  Created by Daniel Coyle on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Beat_The_DeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
