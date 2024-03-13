3c2895770c92c5b12e50abb92c4960abdc023cba
CoyleWasHere <dancoyle321@gmail.com>
2024-03-12 22:41:30 -0400

    - add cards to playview - add working functionality to playview

---
+++ Beat The Deck/MenuView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuView.swift
+//  Beat The Deck
+//
+//  Created by Daniel Coyle on 3/12/24.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuView()
+}
--- Beat The Deck/PlayView.swift
+++ Beat The Deck/PlayView.swift
@@ -14,6 +14,13 @@ struct PlayView: View {
     @State var totalCards = 52
     @State var showSheet = false
     @State var cards = ["card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "card11", "card12", "card13", "card14"]
+    @State var faceCard = Int.random(in: 2...14)
+    @State var drawnCard = Int.random(in: 2...14)
+    
+    
+    @State private var backAlert = false
+    
+    
     
     var body: some View {
         VStack{
@@ -24,12 +31,24 @@ struct PlayView: View {
                     .foregroundStyle(.white)
                 HStack{
                     Button{
-                        // Display InformationView Modally
-                        dismiss()
+                        // Go Back One Page
+                        backAlert = true
                     }label: {
                         Image(systemName: "arrowshape.backward.circle.fill")
                             .font(.largeTitle)
                             .foregroundStyle(.black)
+                    }
+                    .alert("Are you sure you want to go back?", isPresented: $backAlert) {
+                        Button{
+                            dismiss()
+                        }label: {
+                            Text("Go Back")
+                        }
+                        Button{
+                            dismiss()
+                        }label: {
+                            Text("Continue Playing")
+                        }
                     }
                     Spacer()
                 }
@@ -51,28 +70,111 @@ struct PlayView: View {
             Spacer()
             
             // TODO: Deal Cards and create game functionality
-//            Button {
-//            } label: {
-//                ZStack{
-//                    Rectangle()
-//                        .frame(width: 250, height: 110)
-//                        .clipShape(RoundedRectangle(cornerRadius: 110))
-//                        .foregroundStyle(Color(red: 255/255, green: 165/255, blue: 0/255))
-//                    Text("Deal Cards")
-//                        .foregroundStyle(.white)
-//                        .font(.title)
-//                        .fontWeight(.semibold)
-//                }
-//            }
+            //            Button {
+            //            } label: {
+            //                ZStack{
+            //                    Rectangle()
+            //                        .frame(width: 250, height: 110)
+            //                        .clipShape(RoundedRectangle(cornerRadius: 110))
+            //                        .foregroundStyle(Color(red: 255/255, green: 165/255, blue: 0/255))
+            //                    Text("Deal Cards")
+            //                        .foregroundStyle(.white)
+            //                        .font(.title)
+            //                        .fontWeight(.semibold)
+            //                }
+            //            }
             
-            LazyVGrid(columns: [GridItem(spacing:20), GridItem(spacing:20), GridItem(spacing:20)], content: {
+            LazyVGrid(columns: [GridItem(spacing:20), GridItem(spacing:20), GridItem(spacing:20)], spacing: 20, content: {
+                
+                if faceCard != 1{
+                    Menu{
+                        Button{
+                            // Draw Card
+                            // If card is higher leave new card on top of deck
+                            if faceCard < drawnCard {
+                                faceCard = drawnCard
+                                // Decrement Cards remaining
+                                totalCards -= 1
+                                print("The Next Card Was Higher")
+                            } else {
+                                // If card is equal or lower flip deck over to back of card
+                                faceCard = 1
+                                print("The Next Card Was Lower \(drawnCard)")
+                            }
+                            
+                            // Check if cards remaining is 0
+                            if totalCards == 0 {
+                                // DISPLAY WIN SCREEN
+                            }
+                        }label: {
+                            Text("Higher")
+                        }
+                        Button{
+                            // Draw Card
+                            // If card is lower leave new card on top of deck
+                            if faceCard > drawnCard {
+                                faceCard = drawnCard
+                                // Decrement Cards remaining
+                                totalCards -= 1
+                                print("The Next Card Was Lower")
+                            } else {
+                                // If card is equal or lower flip deck over to back of card
+                                faceCard = 1
+                                print("The Next Card Was Higher \(drawnCard)")
+                            }
+                            
+                            // Check if cards remaining is 0
+                            if totalCards == 0 {
+                                // DISPLAY WIN SCREEN
+                            }
+                        }label: {
+                            Text("Lower")
+                        }
+                        Button{
+                            print("Choice Cancelled")
+                        }label: {
+                            Text("Cancel")
+                        }
+                    } label: {
+                        Image("card\(faceCard)")
+                            .resizable()
+                            .frame(width: 100, height: 156)
+                    }
+                } else {
+                    Image("card\(faceCard)")
+                        .resizable()
+                        .frame(width: 100, height: 156)
+                }
+                
+                Image("card3")
+                    .resizable()
+                    .frame(width: 100, height: 156)
+                Image("card4")
+                    .resizable()
+                    .frame(width: 100, height: 156)
+                Image("card5")
+                    .resizable()
+                    .frame(width: 100, height: 156)
+                Image("card6")
+                    .resizable()
+                    .frame(width: 100, height: 156)
+                Image("card7")
+                    .resizable()
+                    .frame(width: 100, height: 156)
+                Image("card8")
+                    .resizable()
+                    .frame(width: 100, height: 156)
+                Image("card9")
+                    .resizable()
+                    .frame(width: 100, height: 156)
+                Image("card10")
+                    .resizable()
+                    .frame(width: 100, height: 156)
+                
                 
             })
-            
-            // TODO: Use for loop to place 9 random cards from cards array onto the canvas
-            //            for cards in 1..<8 {
-            //                print(cards)
-            //            }
+            .padding()
+            Spacer()
             
             Text("Cards Remaining: \(totalCards)")
                 .foregroundStyle(.white)
