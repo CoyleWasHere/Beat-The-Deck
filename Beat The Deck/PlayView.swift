a35d69f8d65da65d7d178e489d7f8b7a1f06c0dd
CoyleWasHere <dancoyle321@gmail.com>
2024-03-13 22:45:27 -0400

    - Create working functionality to play Beat the Deck - Remove unnecessary code

--- Beat The Deck/PlayView.swift
+++ Beat The Deck/PlayView.swift
@@ -13,18 +13,32 @@ struct PlayView: View {
     
     @State var totalCards = 52
     @State var showSheet = false
-    @State var cards = ["card2", "card3", "card4", "card5", "card8", "card10", "card11", "card13", "card14"]
-    @State var flipCard = 0
-    
     @State private var backAlert = false
+    @State private var showDeal = true
+    @State private var restartAlert = false
+    
+    
+    var deck = ["card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "card11", "card12", "card13", "card14"]
+    @State var gameCards:[String] = []
+    @State var cardCount = 0
+    @State var gameNumber = 0
+    @State var newNumber = 0
+    
     
     var body: some View {
+        
+        let newCard = Int.random(in: 0..<deck.count)
+        
         VStack{
+            
+            // Title
             ZStack{
                 Text("Beat The Deck")
                     .font(.largeTitle)
                     .bold()
                     .foregroundStyle(.white)
+                
+                // Back Button
                 HStack{
                     Button{
                         // Go Back One Page
@@ -41,7 +55,7 @@ struct PlayView: View {
                             Text("Go Back")
                         }
                         Button{
-                            dismiss()
+                            
                         }label: {
                             Text("Continue Playing")
                         }
@@ -49,6 +63,8 @@ struct PlayView: View {
                     Spacer()
                 }
                 .padding()
+                
+                // How to Play
                 HStack{
                     Spacer()
                     Button{
@@ -65,127 +81,175 @@ struct PlayView: View {
             
             Spacer()
             
-            // TODO: Deal Cards and create game functionality
-            //            Button {
-            //            } label: {
-            //                ZStack{
-            //                    Rectangle()
-            //                        .frame(width: 250, height: 110)
-            //                        .clipShape(RoundedRectangle(cornerRadius: 110))
-            //                        .foregroundStyle(Color(red: 255/255, green: 165/255, blue: 0/255))
-            //                    Text("Deal Cards")
-            //                        .foregroundStyle(.white)
-            //                        .font(.title)
-            //                        .fontWeight(.semibold)
-            //                }
-            //            }
-            
+            // Display the cards in a Grid
             LazyVGrid(columns: [GridItem(spacing:20), GridItem(spacing:20), GridItem(spacing:20)], spacing: 20, content: {
                 
                 // Select 9 cards from the array and place them in the grid
-                ForEach(Array(cards.enumerated()), id:\.element) { index, card in
+                ForEach(gameCards.indices, id: \.self) { index in
                     
-                    var faceCard = Int.random(in: 2...14)
-                    let drawnCard = Int.random(in: 2...14)
-                    
-                    if flipCard != 1 {
-                        Menu{
-                            Button{
-                                // Draw Card
-                                // If card is higher leave new card on top of deck
-                                if faceCard < drawnCard {
-                                    // Decrement Cards remaining
-                                    totalCards -= 1
-                                    print("The Next Card Was Higher \(drawnCard)")
-                                    print(card)
-                                    print(index)
-                                } else {
-                                    // If card is equal or lower flip deck over to back of card
-                                    flipCard = 1
-                                    print("The Next Card Was Lower or Equal \(drawnCard)")
-                                    print(card)
-                                    print(index)
+                    if totalCards != 0 {
+                        Menu {
+                            if gameCards[index] != "card1" {
+                                Button {
+                                    
+                                    if let gameNumber = Int(gameCards[index].components(separatedBy: CharacterSet.decimalDigits.inverted).joined()) {
+                                        // Do something with this number
+                                        print(gameNumber)
+                                        self.gameNumber = gameNumber
+                                    }
+                                    
+                                    print("Higher")
+                                    
+                                    gameCards.insert(deck[newCard], at: index)
+                                    
+                                    gameCards.remove(at: index)
+                                    
+                                    gameCards[index] = deck[newCard]
+                                    
+                                    if let newNumber = Int(gameCards[index].components(separatedBy: CharacterSet.decimalDigits.inverted).joined()) {
+                                        // Do something with this number
+                                        print(newNumber)
+                                        self.newNumber = newNumber
+                                    }
+                                    
+                                    if gameNumber < newNumber {
+                                        print("Winner")
+                                        totalCards -= 1
+                                    } else {
+                                        print("Loser")
+                                        gameCards[index] = "card1"
+                                    }
+                                    
+                                    
+                                } label: {
+                                    Text("Higher")
                                 }
-                                
-                                // Check if cards remaining is 0
-                                if totalCards == 0 {
-                                    // DISPLAY WIN SCREEN
+                                Button {
+                                    
+                                    if let gameNumber = Int(gameCards[index].components(separatedBy: CharacterSet.decimalDigits.inverted).joined()) {
+                                        // Do something with this number
+                                        print(gameNumber)
+                                        self.gameNumber = gameNumber
+                                    }
+                                    
+                                    print("Lower")
+                                    
+                                    gameCards.insert(deck[newCard], at: index)
+                                    
+                                    gameCards.remove(at: index)
+                                    
+                                    gameCards[index] = deck[newCard]
+                                    
+                                    if let newNumber = Int(gameCards[index].components(separatedBy: CharacterSet.decimalDigits.inverted).joined()) {
+                                        // Do something with this number
+                                        print(newNumber)
+                                        self.newNumber = newNumber
+                                    }
+                                    
+                                    if gameNumber > newNumber {
+                                        print("Winner")
+                                        totalCards -= 1
+                                    } else {
+                                        print("Loser")
+                                        gameCards[index] = "card1"
+                                    }
+                                    
+                                    
+                                } label: {
+                                    Text("Lower")
                                 }
-                            }label: {
-                                Text("Higher")
+                                Button{
+                                    
+                                } label: {
+                                    Text("Cancel")
+                                }
                             }
-                            Button{
-                                // Draw Card
-                                // If card is lower leave new card on top of deck
-                                if faceCard > drawnCard {
-                                    faceCard = drawnCard
-                                    // Decrement Cards remaining
-                                    totalCards -= 1
-                                    print("The Next Card Was Lower \(drawnCard)")
-                                } else {
-                                    // If card is equal or lower flip deck over to back of card
-                                    flipCard = 1
-                                    print("The Next Card Was Higher or Equal \(drawnCard)")
-                                }
-                                
-                                // Check if cards remaining is 0
-                                if totalCards == 0 {
-                                    // DISPLAY WIN SCREEN
-                                }
-                            }label: {
-                                Text("Lower")
-                            }
-                            Button{
-                                print("Choice Cancelled")
-                            }label: {
-                                Text("Cancel")
-                            }
+                            
                         } label: {
-                            Image("card\(faceCard)")
+                            Image(gameCards[index])
                                 .resizable()
                                 .frame(width: 100, height: 156)
                         }
-
-                    } else {
-                        Image("card\(1)")
-                            .resizable()
-                            .frame(width: 100, height: 156)
                     }
                 }
-                
-                // Add the functionality to each card
-//                Group{
-//                    Image("card3")
-//                        .resizable()
-//                        .frame(width: 100, height: 156)
-//                    Image("card4")
-//                        .resizable()
-//                        .frame(width: 100, height: 156)
-//                    Image("card5")
-//                        .resizable()
-//                        .frame(width: 100, height: 156)
-//                    Image("card6")
-//                        .resizable()
-//                        .frame(width: 100, height: 156)
-//                    Image("card7")
-//                        .resizable()
-//                        .frame(width: 100, height: 156)
-//                    Image("card8")
-//                        .resizable()
-//                        .frame(width: 100, height: 156)
-//                    Image("card9")
-//                        .resizable()
-//                        .frame(width: 100, height: 156)
-//                    Image("card10")
-//                        .resizable()
-//                        .frame(width: 100, height: 156)
-//                }
             })
             .padding()
+            
+            // If you win, show a winning message!!
+            if totalCards <= 0 {
+                ZStack{
+                    Rectangle()
+                        .frame(width: 400, height: 220)
+                        .clipShape(RoundedRectangle(cornerRadius: 110))
+                        .foregroundStyle(Color(red: 12/255, green: 234/255, blue: 17/255))
+                    Text("Congratulations . . . You Win")
+                        .foregroundStyle(.white)
+                        .font(.title)
+                        .fontWeight(.semibold)
+                }
+            }
+            
             Spacer()
             
             Text("Cards Remaining: \(totalCards)")
                 .foregroundStyle(.white)
+                .font(.title)
+            
+            // Deal Cards
+            if showDeal == true {
+                Button {
+                    withAnimation {
+                        dealCards()
+                        showDeal = false
+                        totalCards -= 9
+                    }
+                } label: {
+                    ZStack{
+                        Rectangle()
+                            .frame(width: 250, height: 110)
+                            .clipShape(RoundedRectangle(cornerRadius: 110))
+                            .foregroundStyle(Color(red: 12/255, green: 234/255, blue: 17/255))
+                        Text("Deal")
+                            .foregroundStyle(.white)
+                            .font(.title)
+                            .fontWeight(.semibold)
+                    }
+                }
+            }
+            
+            // Restart Game
+            if showDeal == false {
+                Button{
+                    restartAlert = true
+                } label: {
+                    ZStack{
+                        Rectangle()
+                            .frame(width: 250, height: 110)
+                            .clipShape(RoundedRectangle(cornerRadius: 110))
+                            .foregroundStyle(Color(red: 255/255, green: 165/255, blue: 0/255))
+                        Text("Restart")
+                            .foregroundStyle(.white)
+                            .font(.title)
+                            .fontWeight(.semibold)
+                    }
+                }
+                .alert("Are you sure you want to restart?", isPresented: $restartAlert) {
+                    Button {
+                        withAnimation {
+                            gameCards = []
+                            showDeal = true
+                            totalCards = 52
+                        }
+                    } label: {
+                        Text("Restart")
+                    }
+                    Button {
+                    } label: {
+                        Text("Continue Playing")
+                    }
+
+                }
+            }
         }
         .background(Image("background-cloth"))
         .toolbar(.hidden, for: .navigationBar)
@@ -194,6 +258,18 @@ struct PlayView: View {
                 .presentationDetents([.fraction(0.90)])
         })
     }
+    
+    // Get a random array of nine cards
+    func dealCards() {
+        repeat {
+            let randomCard = Int.random(in: 0..<deck.count)
+            gameCards.append(deck[randomCard])
+            cardCount += 1
+        } while cardCount < 9
+        print(gameCards)
+        cardCount = 0
+    }
+    
 }
 
 #Preview {
