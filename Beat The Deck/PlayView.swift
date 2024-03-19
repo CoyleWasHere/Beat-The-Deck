fd001481c6fafcd1ddea31a4437a59494978c0a1
CoyleWasHere <dancoyle321@gmail.com>
2024-03-18 20:30:24 -0400

    - Crash when win game fix - Add win game sound

--- Beat The Deck/PlayView.swift
+++ Beat The Deck/PlayView.swift
@@ -13,13 +13,13 @@ struct PlayView: View {
     @Environment(\.dismiss) private var dismiss
     @State var audioPlayer:AVAudioPlayer?
     
-    @State var totalCards = 52
+    @State var totalCards = 12
     @State var showSheet = false
     @State private var backAlert = false
     @State private var showDeal = true
     @State private var restartAlert = false
     
-    @State var deck = ["clubs_2", "clubs_3", "clubs_4", "clubs_5", "clubs_6", "clubs_7", "clubs_8", "clubs_9", "clubs_10", "clubs_11", "clubs_12", "clubs_13", "clubs_14", "diamonds_2", "diamonds_3", "diamonds_4", "diamonds_5", "diamonds_6", "diamonds_7", "diamonds_8", "diamonds_9", "diamonds_10", "diamonds_11", "diamonds_12", "diamonds_13", "diamonds_14", "hearts_2", "hearts_3", "hearts_4", "hearts_5", "hearts_6", "hearts_7", "hearts_8", "hearts_9", "hearts_10", "hearts_11", "hearts_12", "hearts_13", "hearts_14", "spades_2", "spades_3", "spades_4", "spades_5", "spades_6", "spades_7", "spades_8", "spades_9", "spades_10", "spades_11", "spades_12", "spades_13", "spades_14"]
+    @State var deck = ["clubs_2", "clubs_3", "clubs_4", "clubs_5", "clubs_6", "clubs_7", "clubs_8", "clubs_9", "clubs_10", "clubs_11", "clubs_12", "clubs_13", "clubs_14", "diamonds_2", "diamonds_3", "diamonds_4", "diamonds_5", "diamonds_6", "diamonds_7", "diamonds_8", "diamonds_9", "diamonds_10", "diamonds_11", "diamonds_12", "diamonds_13", "diamonds_14", "hearts_2", "hearts_3", "hearts_4", "hearts_5", "hearts_6", "hearts_7", "hearts_8", "hearts_9", "hearts_10", "hearts_11", "hearts_12", "hearts_13", "hearts_14", "spades_2", "spades_3", "spades_4", "spades_5", "spades_6", "spades_7", "spades_8", "spades_9", "spades_10", "spades_11", "spades_12", "spades_13", "spades_14", "spades_14"]
     
     @State var gameCards:[String] = []
     @State var cardCount = 0
@@ -209,6 +209,10 @@ struct PlayView: View {
                         .font(.title)
                         .fontWeight(.semibold)
                 }
+                .onAppear(perform: {
+                    winner()
+                    audioPlayer?.play()
+                })
             }
             
             Spacer()
@@ -260,7 +264,7 @@ struct PlayView: View {
                 .alert("Are you sure you want to restart?", isPresented: $restartAlert) {
                     Button {
                         withAnimation {
-                            deck = ["clubs_2", "clubs_3", "clubs_4", "clubs_5", "clubs_6", "clubs_7", "clubs_8", "clubs_9", "clubs_10", "clubs_11", "clubs_12", "clubs_13", "clubs_14", "diamonds_2", "diamonds_3", "diamonds_4", "diamonds_5", "diamonds_6", "diamonds_7", "diamonds_8", "diamonds_9", "diamonds_10", "diamonds_11", "diamonds_12", "diamonds_13", "diamonds_14", "hearts_2", "hearts_3", "hearts_4", "hearts_5", "hearts_6", "hearts_7", "hearts_8", "hearts_9", "hearts_10", "hearts_11", "hearts_12", "hearts_13", "hearts_14", "spades_2", "spades_3", "spades_4", "spades_5", "spades_6", "spades_7", "spades_8", "spades_9", "spades_10", "spades_11", "spades_12", "spades_13", "spades_14"]
+                            deck = ["clubs_2", "clubs_3", "clubs_4", "clubs_5", "clubs_6", "clubs_7", "clubs_8", "clubs_9", "clubs_10", "clubs_11", "clubs_12", "clubs_13", "clubs_14", "diamonds_2", "diamonds_3", "diamonds_4", "diamonds_5", "diamonds_6", "diamonds_7", "diamonds_8", "diamonds_9", "diamonds_10", "diamonds_11", "diamonds_12", "diamonds_13", "diamonds_14", "hearts_2", "hearts_3", "hearts_4", "hearts_5", "hearts_6", "hearts_7", "hearts_8", "hearts_9", "hearts_10", "hearts_11", "hearts_12", "hearts_13", "hearts_14", "spades_2", "spades_3", "spades_4", "spades_5", "spades_6", "spades_7", "spades_8", "spades_9", "spades_10", "spades_11", "spades_12", "spades_13", "spades_14", "spades_14"]
                             gameCards = []
                             showDeal = true
                             totalCards = 52
@@ -329,7 +333,7 @@ struct PlayView: View {
     
     func winner() {
         // Load the audio file
-        if let soundURL = Bundle.main.url(forResource: "winner", withExtension: "wav") {
+        if let soundURL = Bundle.main.url(forResource: "Win", withExtension: "mp3") {
             do {
                 audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
                 audioPlayer?.prepareToPlay()
